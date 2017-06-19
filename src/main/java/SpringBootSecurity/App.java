9d958a54ec5a7d2ac8be556e64e64f6b98f18092
theoneLee <623507020@qq.com>
2017-06-19 11:05:11 +0800

    初始化项目（如：pom等等）

---
+++ src/main/java/SpringBootSecurity/App.java
@@ -0,0 +1,14 @@
+package SpringBootSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Lee on 2017/6/19 0019.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
